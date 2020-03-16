f10a6e3bd5a305bb231f1e295cd9f5d2f9078192
Lukas Bernreiter <lukas.bernreiter@ieee.org>
2020-03-16 15:16:57 +0100

    wip on the separation of training and test data

--- script/data_source.py
+++ script/data_source.py
@@ -5,12 +5,16 @@ import open3d as o3d
 from os import listdir
 
 class DataSource:
-    def __init__(self, path_to_datasource):
+    def __init__(self, path_to_datasource, training_split = 0.8):
         self.datasource = path_to_datasource
-        self.anchors = None
-        self.positives = None
-        self.negatives = None
+        self.anchors_training = None
+        self.positives_training = None
+        self.negatives_training = None
+        self.anchors_test = None
+        self.positives_test = None
+        self.negatives_test = None
         self.ds_size = 0
+        self.training_split = training_split
 
     def load(self, n = -1):
         path_anchor = self.datasource + "/training_anchor/"
@@ -18,17 +22,20 @@ class DataSource:
         path_negatives = self.datasource + "/training_negative/"
 
         print("Loading anchors from:\t", path_anchor)
-        self.anchors = self.loadDataset(path_anchor, n)
+        anchors = self.loadDataset(path_anchor, n)
         print("Loading positives from:\t", path_positives)
-        self.positives = self.loadDataset(path_positives, n)
+        positives = self.loadDataset(path_positives, n)
         print("Loading negatives from:\t", path_negatives)
-        self.negatives = self.loadDataset(path_negatives, n)
+        negatives = self.loadDataset(path_negatives, n)
+
+        print("Splitting up training and testing data.")
+        self.splitDataset(anchors, positives, negatives)
 
         print("Done loading dataset.")
-        print("\tAnchors: \t", len(self.anchors))
-        print("\tPositives: \t", len(self.positives))
-        print("\tNegatives: \t", len(self.negatives))
-        self.ds_size = len(self.anchors)
+        print(f"\tAnchors total: \t\t{len(anchors)}\t training/test: ({len(self.anchors_training)}/{len(self.anchors_test)})")
+        print(f"\tPositives total: \t{len(positives)}\t training/test: ({len(self.positives_training)}/{len(self.positives_test)})")
+        print(f"\tNegatives total: \t{len(negatives)}\t training/test: ({len(self.negatives_training)}/{len(self.negatives_test)})")
+        self.ds_size = len(self.anchors_training)
 
     def loadDataset(self, path_to_dataset, n):
         idx = 0
@@ -49,6 +56,19 @@ class DataSource:
             if n != -1 and idx >= n:
                 break
         return dataset
+
+    def splitDataset(self, anchors, positives, negatives):
+        n_total = len(anchors)
+        n_training = round(n_total * self.training_split)
+
+        self.anchors_training = anchors[0:n_training]
+        self.anchors_test = anchors[n_training:n_total]
+
+        self.positives_training = anchors[0:n_training]
+        self.positives_test = anchors[n_training:n_total]
+
+        self.negatives_training = anchors[0:n_training]
+        self.negatives_test = anchors[n_training:n_total]
 
     def size(self):
         return self.ds_size
--- script/training_set.py
+++ script/training_set.py
@@ -4,10 +4,17 @@ from dh_grid import DHGrid
 from sphere import Sphere
 
 class TrainingSet(torch.utils.data.Dataset):
-    def __init__(self, data_source, bw=50):
+    def __init__(self, data_source, bw=100, training=True):
         self.ds = data_source
         self.bw = bw
-        self.anchor_features, self.positive_features, self.negative_features, = self.__genAllFeatures(bw)
+        self.is_training = training
+
+        if training:
+            (a,p,n) = (self.ds.anchors_training, self.ds.positives_training, self.ds.negatives_training)
+        else:
+            (a,p,n) = (self.ds.anchors_test, self.ds.positives_test, self.ds.negatives_test)
+
+        self.anchor_features, self.positive_features, self.negative_features = self.__genAllFeatures(bw, a, p, n)
 
     def __getitem__(self, index):
         anchor = torch.from_numpy(self.anchor_features[index])
@@ -18,23 +25,26 @@ class TrainingSet(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.anchor_features)
 
-    def __genAllFeatures(self, bw):
-        n_ds = self.ds.size()
+    def __genAllFeatures(self, bw, anchors, positives, negatives):
+        n_ds = len(anchors)
         grid = DHGrid.CreateGrid(bw)
 
         anchor_features = [None] * n_ds
         positive_features = [None] * n_ds
         negative_features = [None] * n_ds
         for i in range(n_ds):
-            anchor_sphere = Sphere(self.ds.anchors[i])
-            positive_sphere = Sphere(self.ds.positives[i])
-            negative_sphere = Sphere(self.ds.negatives[i])
+            anchor_sphere = Sphere(anchors[i])
+            positive_sphere = Sphere(positives[i])
+            negative_sphere = Sphere(negatives[i])
 
             anchor_features[i] = anchor_sphere.sampleUsingGrid(grid)
             positive_features[i] = positive_sphere.sampleUsingGrid(grid)
             negative_features[i] = negative_sphere.sampleUsingGrid(grid)
 
         return anchor_features, positive_features, negative_features
+
+    def isTraining(self):
+        return self.is_training
 
 
 if __name__ == "__main__":
